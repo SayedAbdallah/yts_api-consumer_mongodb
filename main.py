31007718c029af111104452134ca871b0fe48ce9
SayedAdallah <sayedabdallahfcih@yahoo.com>
2021-03-08 11:43:45 +0200

    change format

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import configparser
 from YTS_Consumer.YTSConsumer import YTSConsumer
 from pymongo import MongoClient
 from typing import Dict
-
+import json
 
 BASE_URL = "https://yts.mx/api/v2/list_movies.json?limit={}&page={}"
 LIMIT = 50
@@ -12,6 +12,29 @@ def read_config() -> Dict:
     conf = configparser.ConfigParser()
     conf.read('yts.ini')
     return dict(conf['DEFAULT'].items())
+
+
+def _format_movie(movie: Dict) -> Dict:
+    new_movie = {
+        'id': movie['id'],
+        'url': movie.get('url'),
+        'imdb_code': movie.get('imdb_code'),
+        'title': movie.get('title'),
+        'title_english': movie.get('title_english'),
+        'title_long': movie.get('title_long'),
+        'slug': movie.get('slug'),
+        'release_year': movie.get('year'),
+        'rating': movie.get('rating'),
+        'runtime': movie.get('runtime'),
+        'synopsis': movie.get('synopsis'),
+        'trailer_code': movie.get('yt_trailer_code'),
+        'language': movie.get('language'),
+        'mpa_rating': movie.get('mpa_rating'),
+        'genres': movie.get('genres', []),
+        'torrents': movie.get('torrents', [])
+    }
+
+    return new_movie
 
 
 def start_consuming(config: Dict) -> None:
@@ -24,6 +47,7 @@ def start_consuming(config: Dict) -> None:
             print(f'Start to scrap page {page}')
             url = BASE_URL.format(LIMIT, page)
             movies = YTSConsumer.consume(url=url)
+            movies = list(map(_format_movie, movies))
             result = db.movies.insert_many(documents=movies)
             print(f'{len(result.inserted_ids)} Movies inserted')
             page += 1
